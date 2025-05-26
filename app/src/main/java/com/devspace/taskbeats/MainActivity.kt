94744c2c877385dd27477731c582f3e065654c30
renanborges <renan.knx@gmail.com>
2025-05-26 20:32:45 +0100

    Removi tudo que e fixo do app, os valores agora sao 100% do banco

--- app/src/main/java/com/devspace/taskbeats/MainActivity.kt
+++ app/src/main/java/com/devspace/taskbeats/MainActivity.kt
@@ -30,8 +30,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        insertDefaultCategory()
-        insertDefaultTask()
 
         val rvCategory = findViewById<RecyclerView>(R.id.rv_categories)
         val rvTask = findViewById<RecyclerView>(R.id.rv_tasks)
@@ -40,55 +38,30 @@ class MainActivity : AppCompatActivity() {
         val categoryAdapter = CategoryListAdapter()
 
         categoryAdapter.setOnClickListener { selected ->
-            val categoryTemp = categories.map { item ->
-                when {
-                    item.name == selected.name && !item.isSelected -> item.copy(isSelected = true)
-                    item.name == selected.name && item.isSelected -> item.copy(isSelected = false)
-                    else -> item
-                }
-            }
+//             val categoryTemp = categories.map { item ->
+//                when {
+//                    item.name == selected.name && !item.isSelected -> item.copy(isSelected = true)
+//                    item.name == selected.name && item.isSelected -> item.copy(isSelected = false)
+//                    else -> item
+//                }
+//            }
 
-            val taskTemp =
-                if (selected.name != "ALL") {
-                    tasks.filter { it.category == selected.name }
-                } else {
-                    tasks
-                }
-            taskAdapter.submitList(taskTemp)
-
-            categoryAdapter.submitList(categoryTemp)
+//            val taskTemp =
+//                if (selected.name != "ALL") {
+//                    tasks.filter { it.category == selected.name }
+//                } else {
+//                    tasks
+//                }
+//            taskAdapter.submitList(taskTemp)
+//
+//            categoryAdapter.submitList(categoryTemp)
         }
 
         rvCategory.adapter = categoryAdapter
         getCategoriesFromDataBase(categoryAdapter)
 
         rvTask.adapter = taskAdapter
-        taskAdapter.submitList(tasks)
-    }
-
-    private fun insertDefaultCategory() {
-        val categoriesEntity = categories.map {
-            CategoryEntity(
-                name = it.name,
-                isSelected = it.isSelected
-            )
-        }
-
-        GlobalScope.launch(Dispatchers.IO) {
-            categoryDao.insertAll(categoriesEntity)
-        }
-    }
-
-    private fun insertDefaultTask() {
-        val taskEntities = tasks.map {
-            TaskEntity(
-                name = it.name,
-                category = it.category
-            )
-        }
-        GlobalScope.launch(Dispatchers.IO) {
-            taskDao.insertAll(taskEntities)
-        }
+        getTasksFromDataBase(taskAdapter)
     }
 
 
@@ -104,78 +77,18 @@ class MainActivity : AppCompatActivity() {
             adapter.submitList(categoriesUiData)
         }
     }
+
+    private fun getTasksFromDataBase(adapter: TaskListAdapter) {
+        GlobalScope.launch(Dispatchers.IO) {
+            val tasksFromDb: List<TaskEntity> = taskDao.getAll()
+            val tasksUiData = tasksFromDb.map {
+                TaskUiData(
+                    name = it.name,
+                    category = it.category
+                )
+            }
+
+            adapter.submitList(tasksUiData)
+        }
+    }
 }
-
-val categories = listOf(
-    CategoryUiData(
-        name = "ALL",
-        isSelected = false
-    ),
-    CategoryUiData(
-        name = "STUDY",
-        isSelected = false
-    ),
-    CategoryUiData(
-        name = "WORK",
-        isSelected = false
-    ),
-    CategoryUiData(
-        name= "WELLNESS",
-        isSelected = false
-    ),
-    CategoryUiData(
-        name= "HOME",
-        isSelected = false
-    ),
-    CategoryUiData(
-        name= "HEALTH",
-        isSelected = false
-    )
-)
-
-val tasks = listOf(
-    TaskUiData(
-        "Ler 10 páginas do livro atual",
-        "STUDY"
-    ),
-    TaskUiData(
-        "45 min de treino na academia",
-        "HEALTH"
-    ),
-    TaskUiData(
-        "Correr 5km",
-        "HEALTH"
-    ),
-    TaskUiData(
-        "Meditar por 10 min",
-        "WELLNESS"
-    ),
-    TaskUiData(
-        "Silêncio total por 5 min",
-        "WELLNESS"
-    ),
-    TaskUiData(
-        "Descer o livo",
-        "HOME"
-    ),
-    TaskUiData(
-        "Tirar caixas da garagem",
-        "HOME"
-    ),
-    TaskUiData(
-        "Lavar o carro",
-        "HOME"
-    ),
-    TaskUiData(
-        "Gravar aulas DevSpace",
-        "WORK"
-    ),
-    TaskUiData(
-        "Criar planejamento de vídeos da semana",
-        "WORK"
-    ),
-    TaskUiData(
-        "Soltar reels da semana",
-        "WORK"
-    ),
-)
